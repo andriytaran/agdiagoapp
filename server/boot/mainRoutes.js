39c4de88dd52484574032a943c4621716b655594
Andriy Taran <andreitdeveloper@gmail.com>
2018-09-11 22:59:25 -0400

    Updated and Insert the content on Home, Team, Contact US pages

--- server/boot/mainRoutes.js
+++ server/boot/mainRoutes.js
@@ -11,11 +11,28 @@ module.exports = function(app) {
 
     })
 
+    app.get('/profile', function(req, res, next) {
+    	 res.render('profile');
+
+    })
+
+    app.get('/contact', function(req, res, next) {
+    	 res.render('contact');
+    })
+
+    app.get('/faq', function(req, res, next) {
+    	 res.render('faq');
+    })
+
+    app.get('/branding', function(req, res, next) {
+    	 res.render('branding_page');
+    })
+
 
     app.post('/login', function(req, res, next) {
     	 res.render('coachLandng');
 
-    })  
+    })
 
 
 }
